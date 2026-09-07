d39edd3a8b0c84626ce192f2b1f5014c59fcc715
AM3M0RY <mmry2940@gmail.com>
2026-09-06 20:57:12 -0400

    build: restrict ABI to arm64-v8a and update VR assets
    Restrict APK build to arm64-v8a for performance and update OpenMW
    configuration files and input mappings for the VR environment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,10 @@ android {
     versionCode = 45
     versionName = "1.0"
 
+    ndk {
+      abiFilters += listOf("arm64-v8a")
+    }
+
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
